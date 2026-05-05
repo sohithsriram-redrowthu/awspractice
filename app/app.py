35aa0597bfbe33565c862aed49b94fae0e7fd1d7
Chintu <167402239+sohithsriram-redrowthu@users.noreply.github.com>
2026-05-05 22:37:18 +0530

    Added new line to check the images

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello from Flask running in EKS! Handled from github actions!"
-    #return "Hello from Flask running in EKS! Handled from github actions"
+    return "Line 2 is added and pushed to the git repo"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
