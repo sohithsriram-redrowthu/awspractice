39f2637f210ee0d5349b06ec5369edfc1fb0c7f2
sohithsriram-redrowthu <167402239+sohithsriram-redrowthu@users.noreply.github.com>
2026-05-05 23:02:00 +0530

    Combine two return statements in hello function

--- app/app.py
+++ app/app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask running in EKS! Handled from github actions!"
-    return "Line 2 is added and pushed to the git repo"
+    return "Hello from Flask running in EKS! Handled from github actions! Line 2 is added and pushed to the git repo"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
