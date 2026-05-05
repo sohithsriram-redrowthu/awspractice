48feb1cc8584b14cc5f6604131b163752d0e4fe3
sohithsriram-redrowthu <167402239+sohithsriram-redrowthu@users.noreply.github.com>
2026-05-05 21:39:45 +0530

    Update greeting message in Flask app

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask running in EKS! Handled from github actions"
+    return "Hello from Flask running in EKS! Handled from github actions!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
